9a93026d57057814f2c38777ef26bc67a8dab80d
muskanbatra-dev <muskanbatra64@gmail.com>
2021-12-10 01:57:33 +0530

    update

--- app.js
+++ app.js
@@ -6,7 +6,36 @@ var outputBox = document.querySelector("#output-box");
 
 submitBtn.addEventListener("click", submitHandler);
 
-function submitHandler () {
+initialPrice.onkeydown = function(e) {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || (e.keyCode == 8)
+      || (e.keyCode == 110)
+      || e.keyCode == 190)) {
+        return false;
+    }
+}
+
+stocksQuantity.onkeydown = function(e) {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || e.keyCode == 8)) {
+        return false;
+    }
+}
+
+currentPrice.onkeydown = function(e) {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || (e.keyCode == 8)
+      || (e.keyCode == 110)
+      || e.keyCode == 190)) {
+        return false;
+    }
+}
+
+
+function submitHandler() {
     var ip = Number(initialPrice.value);
     var qty = Number(stocksQuantity.value);
     var curr = Number(currentPrice.value);
@@ -14,24 +43,28 @@ function submitHandler () {
     calculateProfitAndLoss(ip, qty, curr);
 }
 
+function decimal (x) {
+    return Number.parseFloat(x).toFixed(2);
+}
+
 function calculateProfitAndLoss(initial, quantity, current) {
     if (initial > current) {
-        var loss = (initial - current) * quantity;
-        var lossPercentage = (loss/initial) * 100;
+        var loss = decimal((initial * quantity) - (current * quantity));
+        var lossPercentage = decimal((loss / (current * quantity)) * 100);
 
-        showOutput(`Hey the loss is ${loss} and the percent is ${lossPercentage}%`)
+        outputBox.textContent = (`Hey the invested amount is ${(initial * quantity)} and loss is ${loss} and the percent is ${lossPercentage}%`)
+        outputBox.style.color = "red"
+
     } else if (current > initial) {
-        var profit = (current + initial) * quantity;
-        var profitPercentage = (profit/initial) * 100;
+        var profit = decimal((current * quantity) - (initial * quantity));
+        var profitPercentage = decimal((profit / (initial * quantity)) * 100);
 
-        showOutput(`"Hey the profit is ${profit} and the percent is ${profitPercentage}%"`)
+        outputBox.textContent = (`Hey the invested amount is ${(initial * quantity)} profit is ${profit} and the percent is ${profitPercentage}%`)
+        outputBox.style.color = "green"
     } else {
 
-        showOutput(`No Pain No Gain and No Gain No Pain`);
+        outputBox.textContent = (`No Pain No Gain and No Gain No Pain`);
+        outputBox.style.color = "white"
     }
 
-}    
-
-function showOutput(message) {
-   outputBox.innerHTML = message;
 }
